6a1a26bf21b66bb2ee96d561cff03304c9e46eaf
Julien Gaston <gaston@codelutin.com>
2017-08-31 12:03:00 +0200

    Ajout du script SQL d'initialisation de la base

---
+++ src/main/resources/initDb.sql
@@ -0,0 +1,29 @@
+-- Database definition
+
+-- Tables
+
+CREATE TABLE IF NOT EXISTS public.settings
+(
+  id INTEGER PRIMARY KEY,
+  location TEXT,
+  tv_times TEXT
+)
+ALTER TABLE public.settings OWNER TO ecd;
+
+CREATE TABLE public.signage_stream
+(
+  id UUID PRIMARY KEY,
+  name text UNIQUE NOT NULL,
+  timing INTEGER,
+  frames TEXT,
+  creation_date_time TIMESTAMP WIHOUT TIME ZONE,
+  last_update_date_time TIMESTAMP WIHOUT TIME ZONE
+)
+ALTER TABLE public.settings OWNER TO ecd;
+
+CREATE TABLE public.person
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT
+)
+ALTER TABLE public.person OWNER TO ecd;
